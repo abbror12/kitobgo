66e21c82bb5a55140d133f63c2d7bbe43d375efb
Abror <yoldashevabror182@gmail.com>
2026-07-09 17:39:27 +0500

    first

---
+++ src/test/java/com/example/kitobgo/KitobgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitobgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitobgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
